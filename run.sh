20844c6f4b4d86ab8091d2fd339a004ed4c7ca57
Evan Heidtmann <evan.heidtmann@gmail.com>
2015-08-12 14:26:15 -0700

    todos

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# TODO: add authorization for s3 urls
+# @see http://stackoverflow.com/questions/1306550/calculating-a-sha-hash-with-a-string-secret-key-in-python
+# @see http://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-auth-using-authorization-header.html
+
 if [ ! -e /data/data.osrm.nodes ]; then
   curl $OSRM_TGZ_URL | pigz -d | tar xv -C /data/
 fi
